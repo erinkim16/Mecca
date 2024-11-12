f774ca7ece0ab4d7c6f8779586f0f11835d3ce24
Alessia <alessia.ruberto04@gmail.com>
2024-11-12 16:04:05 -0500

    Added a prop interface and default props

--- PP2/scriptorium/components/execution/code-area.tsx
+++ PP2/scriptorium/components/execution/code-area.tsx
@@ -2,16 +2,31 @@ import React, { useState, useEffect, useRef } from "react";
 
 import MonacoEditor from "@monaco-editor/react";
 
-export default function CodeArea() {
+interface CodeAreaProps {
+    language?: string;
+    height?: string;
+    width?: string;
+}
+
+export default function CodeArea(props: CodeAreaProps) {
+    // TODO -> set default code to correspond to simple language-specifc code
     const [code, setCode] = useState("");
 
     return (
         <MonacoEditor
-            height="400px"
-            language="javascript"
-            theme="vs-light"
+            height={props.height}
+            width={props.width}
+            language={props.language?.toLowerCase()}
+            theme="vs-dark"
             value={code}
             onChange={(value) => setCode(value || "")}
         />
     );
 }
+
+CodeArea.defaultProps = {
+    language: "python",
+    height: "80vh",
+    width: "60vw"
+};
+
