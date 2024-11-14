360964f366681af01e0f7941194500e61c2be389
Alessia <alessia.ruberto04@gmail.com>
2024-11-14 16:48:14 -0500

    Refactored so exec settings are in their own component

---
+++ PP2/scriptorium/components/execution/exec-settings.tsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import LanguageDropdown from "./language-dropdown";
+
+interface ExecSettingsProps {
+    language: string;
+    setLanguage: React.Dispatch<React.SetStateAction<string>>;
+    code: string; // The code content passed as a prop
+    setCode: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function ExecSettings(props: ExecSettingsProps) {
+    const [output, setOutput] = useState("");
+    const [error, setError] = useState("");
+    const [stdin, setStdin] = useState("");
+
+    const executeAndUpdateOut = async () => {
+        // Clear what's currently outputted
+        setOutput("");
+        setError("");
+
+        // New outputs
+        var codeOutput = await executeCode(props.code, props.language, stdin);
+
+        if (codeOutput.error) {
+            setError(codeOutput.error.toString());
+        }
+
+        if (codeOutput.output) {
+            setOutput(codeOutput.output.toString());
+        }
+    };
+    
+    return <>  
+        <LanguageDropdown language={props.language} setLanguage={props.setLanguage}/>
+
+        <button className="m-4" onClick={() => executeAndUpdateOut()}>Run</button>
+
+        <label>Input:</label>
+        <input type="text" value={stdin} onChange={(e) => setStdin(e.target.value)}/>
+
+        <label>Output:</label>
+
+        <div id="output"> 
+            <p>{output}</p>
+
+            {error && (
+                <p>{error}</p>
+            )}
+        </div>    
+    </>;
+}
+
+async function executeCode(code: string, language: string, stdin: string): Promise<{ output: string, error: string }> {
+    const response = fetch("/api/code-writing-and-execution/execute-code", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code, language, stdin }),
+    });
+
+    var data: { output: string, error: string } = await (await response).json();
+
+    return data;
+}   
--- PP2/scriptorium/pages/execution.tsx
+++ PP2/scriptorium/pages/execution.tsx
@@ -1,75 +1,24 @@
 import React, { useEffect, useState } from "react";
 
 import CodeArea from "../components/execution/code-area"
-import LanguageDropdown from "../components/execution/language-dropdown"
-import { JsonObject } from "@prisma/client/runtime/library";
+import ExecSettings from "../components/execution/exec-settings"
 
 export default function Execution() {
     const [code, setCode] = useState("");
-    const [stdin, setStdin] = useState("");
     const [language, setLanguage] = useState("python");
-    const [output, setOutput] = useState("");
-    const [error, setError] = useState("");
-
-    const executeAndUpdateOut = async () => {
-        // Clear what's currently outputted
-        setOutput("");
-        setError("");
-
-        // New outputs
-        var codeOutput = await executeCode(code, language, stdin);
-
-        if (codeOutput.error) {
-            setError(codeOutput.error.toString());
-        }
-
-        if (codeOutput.output) {
-            setOutput(codeOutput.output.toString());
-        }
-    };
-
     
     return (
         <div className="flex items-center justify-center">
 
             <div className="m-4">
-            <CodeArea language={language} code={code} setCode={setCode}/>   
+                <CodeArea language={language} code={code} setCode={setCode}/>   
             </div>
 
             <div className="exec-options m-4">
-            <LanguageDropdown language={language} setLanguage={setLanguage}/>
-
-            <button className="m-4" onClick={() => executeAndUpdateOut()}>Run</button>
-
-            <label>Input:</label>
-            <input type="text" value={stdin} onChange={(e) => setStdin(e.target.value)}/>
-
-            <label>Output:</label>
-
-            <div id="output"> 
-                <p>{output}</p>
-
-                {error && (
-                    <p>{error}</p>
-                )}
-            </div>    
-
+                <ExecSettings language={language} setLanguage={setLanguage} code={code} setCode={setCode}/>
             </div>
   
         </div> 
     )
 }
 
-async function executeCode(code: string, language: string, stdin: string): Promise<JsonObject> {
-    const response = fetch("/api/code-writing-and-execution/execute-code", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ code, language, stdin }),
-    });
-
-    var data: { output: string, error: string } = await (await response).json();
-
-    return data;
-}   
