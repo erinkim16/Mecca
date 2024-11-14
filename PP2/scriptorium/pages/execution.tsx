a01f3403d1a6b71d3032e823c91b4265e2267d8b
Alessia <alessia.ruberto04@gmail.com>
2024-11-14 17:47:29 -0500

    Added support for mobile on execution

--- PP2/scriptorium/pages/execution.tsx
+++ PP2/scriptorium/pages/execution.tsx
@@ -6,12 +6,32 @@ import ExecSettings from "../components/execution/exec-settings"
 export default function Execution() {
     const [code, setCode] = useState("");
     const [language, setLanguage] = useState("python");
+    const [width, setWidth] = useState(0);
+    const [codeWidth, setCodeWidth] = useState("60vw");
+
+    useEffect(() => {
+        const handleResize = () => {
+          const currentWidth = window.innerWidth;
+    
+          if (currentWidth <= 768) {
+            setCodeWidth("90vw");
+          } else {
+            setCodeWidth("60vw");
+          }
+        };
+    
+        // Run once initially to set the correct width
+        handleResize();
+    
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+      }, []); // Empty dependency array ensures this runs only once
     
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center m-2 flex-col md:flex-row">
 
             <div className="m-4">
-                <CodeArea language={language} code={code} setCode={setCode}/>   
+                <CodeArea width={codeWidth} language={language} code={code} setCode={setCode}/>   
             </div>
 
             <div className="exec-options m-4">
