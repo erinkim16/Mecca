c9c063533eec8d87afd78b1b68de3d05e55eedc6
Alessia <alessia.ruberto04@gmail.com>
2024-11-14 16:27:21 -0500

    Added error message popup

--- PP2/scriptorium/pages/execution.tsx
+++ PP2/scriptorium/pages/execution.tsx
@@ -2,48 +2,74 @@ import React, { useEffect, useState } from "react";
 
 import CodeArea from "../components/execution/code-area"
 import LanguageDropdown from "../components/execution/language-dropdown"
+import { JsonObject } from "@prisma/client/runtime/library";
 
 export default function Execution() {
     const [code, setCode] = useState("");
     const [stdin, setStdin] = useState("");
     const [language, setLanguage] = useState("python");
     const [output, setOutput] = useState("");
+    const [error, setError] = useState("");
 
     const executeAndUpdateOut = async () => {
-        var output = await executeCode(code, language, stdin);
-        setOutput(output);
+        // Clear what's currently outputted
+        setOutput("");
+        setError("");
+
+        // New outputs
+        var codeOutput = await executeCode(code, language, stdin);
+
+        if (codeOutput.error) {
+            setError(codeOutput.error.toString());
+        }
+
+        if (codeOutput.output) {
+            setOutput(codeOutput.output.toString());
+        }
     };
 
     
     return (
-        <>
-        <LanguageDropdown language={language} setLanguage={setLanguage}/>
+        <div className="flex items-center justify-center">
 
-        <button onClick={() => executeAndUpdateOut()}>Run</button>
+            <div className="m-4">
+            <CodeArea language={language} code={code} setCode={setCode}/>   
+            </div>
 
-        <label>Input:</label>
-        <input type="text" value={stdin} onChange={(e) => setStdin(e.target.value)}/>
+            <div className="exec-options m-4">
+            <LanguageDropdown language={language} setLanguage={setLanguage}/>
 
-        <label>Output:</label>
-        <div id="output"> 
-            <p></p>
-        </div>
+            <button className="m-4" onClick={() => executeAndUpdateOut()}>Run</button>
 
-        <CodeArea language={language} code={code} setCode={setCode}/>
-        </> 
+            <label>Input:</label>
+            <input type="text" value={stdin} onChange={(e) => setStdin(e.target.value)}/>
+
+            <label>Output:</label>
+
+            <div id="output"> 
+                <p>{output}</p>
+
+                {error && (
+                    <p>{error}</p>
+                )}
+            </div>    
+
+            </div>
+  
+        </div> 
     )
 }
 
-async function executeCode(code: string, language: string, stdin: string): Promise<string> {
-    // const response = fetch("/api/code-writing-and-execution/execute-code", {
-    //     method: "POST",
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ code, language, stdin }),
-    // });
+async function executeCode(code: string, language: string, stdin: string): Promise<JsonObject> {
+    const response = fetch("/api/code-writing-and-execution/execute-code", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code, language, stdin }),
+    });
 
-    // const data = (await response).json();
-    // return data.output;
-    return "";
+    var data: { output: string, error: string } = await (await response).json();
+
+    return data;
 }   
