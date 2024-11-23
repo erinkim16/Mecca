e0205804a582de653f8a7b35ba6656971482aa74
erinkim16 <125765924+erinkim16@users.noreply.github.com>
2024-11-23 14:27:51 -0500

    testing

--- PP2/scriptorium/middleware.ts
+++ PP2/scriptorium/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-
+// for pushing test
 export function middleware(request: NextRequest) {
   // Get the token from authorization header
   const token = request.headers.get("authorization")?.split(" ")[1];
@@ -9,7 +9,7 @@ export function middleware(request: NextRequest) {
   if (!token && request.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
+  // testing testing
   // For protected API routes
   if (!token && request.nextUrl.pathname.startsWith("/api/protected")) {
     return new NextResponse(
