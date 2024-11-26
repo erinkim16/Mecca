c49de2909f105839af594ab329a161f03df65a5b
Alessia <alessia.ruberto04@gmail.com>
2024-11-25 23:39:28 -0500

    edited startup

--- PP2/startup.sh
+++ PP2/startup.sh
@@ -24,4 +24,17 @@ npm i
 npx prisma generate
 node create-admin.js
 
+cd .. 
+cd temp_code
+docker build -t my-java-env -f Dockerfile.java .
+docker build -t my-python-env -f Dockerfile.python .
+docker build -t my-javascript-env -f Dockerfile.javascript .
+docker build -t my-ruby-env -f Dockerfile.ruby .
+docker build -t my-golang-env -f Dockerfile.golang .
+docker build -t my-perl-env -f Dockerfile.perl . 
+docker build -t my-php-env -f Dockerfile.php .
+docker build -t my-swift-env -f Dockerfile.swift . 
+docker build -t my-elixir-env -f Dockerfile.elixir .
+
+
 echo "Startup Complete"
